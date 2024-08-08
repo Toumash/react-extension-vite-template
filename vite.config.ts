9a0296c9d96613d8850a49955f4cbced52fae1b5
Tomasz Dłuski <mr.toumash@gmail.com>
2024-08-08 19:24:07 +0200

    feat: add sample service_worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx, defineManifest } from '@crxjs/vite-plugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: 'some name',
-  version: '1.0.0',
-  content_scripts: [{
-    js: ['./src/content.tsx'],
-    matches: ['<all_urls>']
-  }],
-  action: {
-    default_popup: 'index.html'
-  }
-})
+  name: "some name",
+  version: "1.0.0",
+  background: {
+    service_worker: "src/background.ts",
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
-})
+});
